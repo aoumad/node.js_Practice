91ad7f4464142f42f6204d480cfcd203dcac703d
aoumad <abderazzakoumad@gmail.com>
2023-03-04 21:58:40 +0100

    done

---
+++ function_expression.js
@@ -0,0 +1,19 @@
+// normal function statement
+function sayHi()
+{
+    console.log("Hi");
+}
+sayHi();
+
+function callFunction(fun)
+{
+    fun();
+}
+
+// function expression
+var sayBye = function()
+{
+    console.log("Bye");
+};
+// sayBye();
+callFunction(sayBye);
